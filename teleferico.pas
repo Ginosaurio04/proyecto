0090b969465b364f28c5964809fcda4a362d62c9
Gino Cova <gcova.1249@unimar.edu.ve>
2024-10-25 01:19:29 -0400

    version 1 proyecto commit

---
+++ teleferico.pas
@@ -0,0 +1,15 @@
+program teleferico;
+var
+capacida maxima, boletos vendidos, monto total, boletos generales vendidos, boletos tercera edad vendidios, boletos menores vendidos: integer
+begin
+  capacidad maxima < 60;
+  boletos vendidos, monto total, boletos generales vendidos, boletos tercera edad vendidos, boletos menores vendidios:
+    while true do;
+      writeln('opcion 1.Comprar boleto');
+      readln (opcion 1)
+      writeln ('opcion 2.Ver sistema');
+      readln (opcion 2)
+      writeln('opcion 3.salir');
+      readln(opcion 3);
+
+end.

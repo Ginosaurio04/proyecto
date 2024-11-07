47c458087f89c45ad25527e5cd6e7c0989fd832c
Gino Cova <gcova.1249@unimar.edu.ve>
2024-11-07 12:18:57 -0400

    coreccion de begins y ends

--- fibonacci.pas
+++ fibonacci.pas
@@ -1,13 +1,18 @@
 program fibonacci;
 var
 i,limite: integer;
-fibonacci: array[1..100] of integer;
+fibonacci:array[1..100] of integer;
 begin
     limite:= 100;
     fibonacci[1]:=0;
     fibonacci[2]:=1;
+
     for i:= 3 to limete do
-    fibonacci[i]:= fibonacci[i-1]+fibonacci[i-2];
+    begin
+        fibonacci[i]:= fibonacci[i-1]+fibonacci[i-2];
+    end;
     for i:= 1 to limite do
-    WriteLn(fibonacci[i]);
-end
+    begin
+        WriteLn(fibonacci[i]);
+    end;
+end. 
